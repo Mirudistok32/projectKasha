6ecb2f7954a04d9cc8a68a1677acee92faec494a
Mirudistok32 <mirudistok32@yandex.ru>
2020-09-02 18:05:05 +0300

    Refactor Content component, add React.lazy

--- src/components/Body/Content/Content.jsx
+++ src/components/Body/Content/Content.jsx
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { Suspense } from "react";
 import s from "./Content.module.scss";
 import { Route, Switch, Redirect } from 'react-router-dom'
 import Home from './pages/Home/Home'
-import ContainerItemPage from './pages/TodoPage//Items/ItemPage/ContainerItemPage'
+// import ContainerItemPage from './pages/TodoPage//Items/ItemPage/ContainerItemPage'
 import ContainerTodoPage from './pages/TodoPage/ContainerTodoPage'
-import CalcFinance from './pages/CalcFinance/CalcFinance'
-import DialogsPage from './pages/DialogsPage/DialogsPage'
+// import CalcFinance from './pages/CalcFinance/CalcFinance'
+// import DialogsPage from './pages/DialogsPage/DialogsPage'
+
+
+const DialogsPage = React.lazy(() => import('./pages/DialogsPage/DialogsPage'));
+const CalcFinance = React.lazy(() => import('./pages/CalcFinance/CalcFinance'));
+const ContainerItemPage = React.lazy(() => import('./pages/TodoPage//Items/ItemPage/ContainerItemPage'));
 
 const Content = () => {
   return (
@@ -13,9 +18,24 @@ const Content = () => {
       <Switch>
         <Route exact path="/" render={() => <Home />} />
         <Route exact path="/todo" render={() => <ContainerTodoPage />} />
-        <Route exact path="/todo/:id?" render={() => <ContainerItemPage />} />
-        <Route exact path="/dialogs" render={() => <DialogsPage />} />
-        <Route exact path="/calculate" render={() => <CalcFinance />} />
+        <Route exact path="/todo/:id?" render={() => (
+          <Suspense fallback={<div>Поготь, идет загрузка</div>}>
+            <ContainerItemPage />
+          </Suspense>
+        )}
+        />
+        <Route exact path="/calculate" render={() => (
+          <Suspense fallback={<div>Поготь, идет загрузка</div>}>
+            <CalcFinance />
+          </Suspense>
+        )}
+        />
+        <Route exact path="/dialogs" render={() => (
+          <Suspense fallback={<div>Поготь, идет загрузка</div>}>
+            <DialogsPage />
+          </Suspense>)}
+        />
+
         <Redirect exact to="/" />
       </Switch>
     </section>
